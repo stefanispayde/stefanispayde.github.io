1dac7cf3012ca94d316e9e6d6c1336347ccfe008
stefani spayde <spayde.stefani@gmail.com>
2017-07-20 10:58:46 -0500

    made corrected changes to compareValues() logic

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(()=> {
 // console.log("What up?");
 
-let point = 0;
+let player1Points = 0;
+let player2Points = 0;
 
 //this is the array/for loop to choose a random letter from the dice
 let rollDie = ['a', 'b', 'c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -24,8 +25,8 @@ const formSubmission = () => {
     let answer = player1Inputs.eq(i).val();
     player1Answers.push(answer);
   }
-  // return player1Answers;
   console.log(player1Answers);
+  $('#player1').css('visibility', 'hidden');
 }
 
 
@@ -37,8 +38,9 @@ const formSubmission2 = () => {
       player2Answers.push(answer2);
     }
     console.log(player2Answers);
+    $('#player2').css('visibility', 'visible');
     compareValues(player1Answers, player2Answers);
-  }
+}
 
 
 // this function gives points to each player for their answers
@@ -47,23 +49,29 @@ const compareValues = (array1, array2) => {
 
   for (let i = 0; i < array2.length; i++ ){
     //this if statement compares answers to ensure they are not the same
-    if (array1[i].toLowerCase().trim() !== array2[i].toLowerCase().trim()) {
-      point += 1;
-      console.log(point);
-    } else {
-      console.log("This answer is a dulicate. No point earned.");
-      }
+    if( array1[i] !== " " && array2[i] !== " "){
+
+      if (array1[i].toLowerCase().trim() !== array2[i].toLowerCase().trim()){
+        player1Points += 1;
+        player2Points += 1;
+        console.log("Player 1, you have recieved " + player1Points + " this round.");
+        console.log("Player 2, you have recieved " + player2Points + " this round.");
+      } else {
+        console.log("This answer is a dulicate. No point earned.");
+        }
+    }
   }
+
 }
 
 //clear board/reset function------can I write this in one function and still clear both forms
-// const clearForm1() {
-//     document.getElementById("player1").reset();
-//   }
-//
-// const clearForm2() {
-//     document.getElementById("player2").reset();
-//   }
+const clearForm1 = () => {
+    $("#player1").reset();
+  }
+
+const clearForm2 = () => {
+    $("#player2").reset();
+  }
 
 // on submit of form 1, save answers into an array-------also need hide function for player1's input before formSubmission2 is called
   $( "#player1" ).submit(function( event ) {
@@ -78,13 +86,9 @@ const compareValues = (array1, array2) => {
   });
 
   //this enables player 1 to hide their answers when it is player 2's turn-----making player 2's form disappear
-  $(document).ready(function(){
-    $("#hide").click(function(){
-        $("#player1").hide();
-    });
-    $("#show").click(function(){
-        $("#player1").show();
-    });
-});
+  // $('#player1').css('visibility', 'hidden');
+  // $('#player2').css('visibility', 'visible');
 
-})
+
+
+});

83f4b71ed46bf40f84ee80221ae7ac570d22eb7a
stefani spayde <spayde.stefani@gmail.com>
2017-07-20 16:10:43 -0500

    completed function for comparing players answer arrays and assigning points for unique answers

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ const formSubmission2 = () => {
     }
     console.log(player2Answers);
     $('#player1').css('visibility', 'visible');
-    // compareValues(player1Answers, player2Answers);
     notEmpty2(player2Answers);
 }
 
@@ -67,15 +66,13 @@ const notEmpty2 = (array2) => {
       console.log("empty string " + numEmpty2);
     }
   }
+  compareValues(array1, array2);//need to grab array1, will be out of scope
 }
 
 
-// this function gives points to each player for their answers
-// const compareValues = (array1, array2) => {
-  // console.log(array1, array2);
-
-
-
+// this function checks to see if players have the same answer on the same question, gives points based on number of unique answers and tallys them
+// const compareValues = (answers1, answers2) => {
+//   console.log(answers1, answers2);
 //   for (let i = 0; i < array2.length; i++ ){
 //     //this if statement compares answers to ensure they are not the same
 //         if (array1[i].toLowerCase().trim() !== array2[i].toLowerCase().trim()){
@@ -85,13 +82,25 @@ const notEmpty2 = (array2) => {
 //           console.log("Player 2, you have recieved " + player2Points + " points this round.");
 //         } else {
 //           console.log("This answer is a dulicate. No point earned.");
-//           } else {
-//       console.log("This question was not answered. No points given.");
-//             }
+//           }
 //   }
-// // clearForm1();
-// // clearForm2();
+// // // // clearForm1();
+// // // // clearForm2();
 // }
+const tallyScore = (player1Points, numEmpty, player2Points, numEmpty2) => {
+  player1Tally = player1Points - numEmpty;
+  player2Tally = player2Points - numEmpty2;
+  console.log("Player 1 has a total of " + player1Tally + " points.");
+  console.log("Player 2 has a total of " + player2Tally + " points.")
+
+    if (player1Tally > player2Tally) {
+      console.log("Player 1 is the winner!");
+    }else{
+      console.log("Player 2 is the winner!");
+    }
+}
+
+
 
 //clear board/reset function------can I write this in one function and still clear both forms
 // const clearForm1 = () => {
@@ -105,7 +114,6 @@ const notEmpty2 = (array2) => {
 // on submit of form 1, save answers into an array-------also need hide function for player1's input before formSubmission2 is called
   $( "#player1" ).submit(function( event ) {
     event.preventDefault();
-    console.log('form 1 submission');
     formSubmission();
   });
 // on submit of form 2, compare values
@@ -113,11 +121,5 @@ const notEmpty2 = (array2) => {
     event.preventDefault();
     formSubmission2();
   });
-
-  //this enables player 1 to hide their answers when it is player 2's turn-----making player 2's form disappear
-  // $('#player1').css('visibility', 'hidden');
-  // $('#player2').css('visibility', 'visible');
-
-
 
 });
